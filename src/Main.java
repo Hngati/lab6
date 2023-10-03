6518b1e5b8b82a4b1f914d4d803ea4ea88f69555
Hngati <145719995+Hngati@users.noreply.github.com>
2023-10-02 21:42:17 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,56 +3,63 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-                Scanner in = new Scanner(System.in); //scanner object
-                double gallons = 0;
-                int efficiency = 0;
-                double price = 0;
-                boolean done = false;
 
-                do {
-                    System.out.print("Enter the number of gallons of gas in the tank: ");
-                    if (in.hasNextDouble()) {
-                        gallons = in.nextDouble();
-                        done = true;
-                    } else {
-                        System.out.println("Invalid input. Please enter a valid number for gallons.");
-                        in.nextLine(); // Clear the newline
-                    }
-                } while (!done);
+        Scanner in = new Scanner(System.in);
 
-                done = false; // Reset
+        double gallons, price;
+        int efficiency;
 
-                do {
-                    System.out.print("Enter the fuel efficiency in miles per gallon: ");
-                    if (in.hasNextInt()) {
-                        efficiency = in.nextInt();
-                        done = true;
-                    } else {
-                        System.out.println("Invalid input. Please enter a valid integer for efficiency.");
-                        in.nextLine(); // Clear the newline from the buffer
-                    }
-                } while (!done);
-
-                done = false; // Reset for the next input
-
-                do {
-                    System.out.print("Enter the price of gas per gallon: ");
-                    if (in.hasNextDouble()) {
-                        price = in.nextDouble();
-                        done = true;
-                    } else {
-                        System.out.println("Invalid input. Please enter a valid number for price.");
-                        in.nextLine(); // Clear the newline
-                    }
-                } while (!done);
-
-                if (gallons > 0 && efficiency > 0 && price > 0) {
-                    System.out.println("Cost per 100 miles for the given gas price " + price + " is " + (price * 100));
-                    System.out.println("The distance (in miles) the car can go with the gas in the tank is " + (int)(efficiency * gallons));
+        while (true) {
+            System.out.print("Enter the number of gallons of gas in the tank: ");
+            if (in.hasNextDouble()) {
+                gallons = in.nextDouble();
+                if (gallons >= 0) {
+                    break;
                 } else {
-                    System.out.println("Invalid input for gallons, efficiency, or price.");
+                    System.out.println("Invalid input. Please enter a non-negative number for gallons.");
                 }
-
-                in.close(); // Close scanner
+            } else {
+                System.out.println("Invalid input. Please enter a valid number for gallons.");
+                in.next(); // Clear the invalid input
             }
         }
+
+        while (true) {
+            System.out.print("Enter the fuel efficiency in miles per gallon: ");
+            if (in.hasNextInt()) {
+                efficiency = in.nextInt();
+                if (efficiency > 0) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter a positive integer for efficiency.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a valid integer for efficiency.");
+                in.next(); // Clear the invalid input
+            }
+        }
+
+        while (true) {
+            System.out.print("Enter the price of gas per gallon: ");
+            if (in.hasNextDouble()) {
+                price = in.nextDouble();
+                if (price >= 0) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter a non-negative number for price.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a valid number for price.");
+                in.next(); // Clear the invalid input
+            }
+        }
+
+        double costPer100Miles = price * 100;
+        double distance = efficiency * gallons;
+
+        System.out.println("Cost per 100 miles for the given gas price " + price + " is " + costPer100Miles);
+        System.out.println("The distance (in miles) the car can go with the gas in the tank is " + distance);
+
+        in.close(); // Close scanner
+    }
+}
